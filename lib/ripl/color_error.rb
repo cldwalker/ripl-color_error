dc0a454fe28e6bd485750c2f1aee5c3f017f1f82
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-09 02:08:31 -0500

    add readme and version

--- lib/ripl/color_error.rb
+++ lib/ripl/color_error.rb
@@ -1,14 +1,16 @@
 module Ripl
   module ColorError
+    VERSION = '0.1.0'
     COLORS = {:red=>31, :green=>32, :yellow=>33, :blue=>34, :purple=>35, :cyan=>36}
+
     Ripl.config[:color_error] ||= :red
 
     def format_error(error)
-      color_code = config[:color_error].to_s[/^[\d;]+$/] ? config[:color_error] :
-        COLORS[config[:color_error].to_sym]
+      color_code = config[:color_error].to_s[/^[\d;]+$/] || !config[:color_error] ?
+        config[:color_error] : COLORS[config[:color_error].to_sym]
       color_code ? "\e[#{color_code}m#{super}\e[m" : super
     end
   end
 end
 
-Ripl::Shell.send :include, Ripl::ColorError
+Ripl::Shell.send :include, Ripl::ColorError if defined? Ripl::Shell
